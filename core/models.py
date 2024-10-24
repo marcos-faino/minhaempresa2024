e922e8eb25432d29fdf832ef41aa895096297ba8
Marcos Alves Faino <1812293@ifro.local>
2024-10-24 19:44:11 -0400

    24-10-2024

--- core/models.py
+++ core/models.py
@@ -25,7 +25,7 @@ class Cargo(models.Model):
 class Funcionario(Base):
     nome = models.CharField(max_length=200)
     bio = models.TextField()
-    foto = StdImageField(upload_to='equipe',
+    foto = StdImageField(upload_to=,
                          variations={'thumb':{'width':500,
                                               'height':500,
                                               'crop':True}})
